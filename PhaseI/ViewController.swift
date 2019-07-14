85224cdd640ee8a9b12ffcff9238ee92015e0197
Lara Riparip <lriparip@gmail.com>
2019-07-14 13:54:20 +0200

    pixelData and single pixelColor output working

--- PhaseI/ViewController.swift
+++ PhaseI/ViewController.swift
@@ -7,14 +7,265 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var camPreview: UIImageView!
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var activeInput: AVCaptureDeviceInput!
+    var viewImage: CGImage?
+    
+    let dataOutputQueue = DispatchQueue(label: "video data queue",
+                                        qos: .userInitiated,
+                                        attributes: [],
+                                        autoreleaseFrequency: .workItem)
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let touchLocation: CGPoint = sender.location(in: sender.view)
+            print (touchLocation)
+            print(camPreview.getPixelColorAtPoint(point: touchLocation, sourceView: camPreview))
+            
+            if viewImage != nil{
+                print(viewImage!.pixelData())
+            } else {
+                print("UIImage is empty")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        func setupPreview() {
+//            // Configure previewLayer
+//            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+//            previewLayer.frame = camPreview.bounds
+//            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//            view.layer.addSublayer(previewLayer)
+//        }
+//
+//        if setupSession() {
+//            setupPreview()
+//        }
+        if setupSession() {
+          captureSession.startRunning()
+        }
+        
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(sender:)))
+        tap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tap)
     }
-
 
 }
 
+extension ViewController {
+    func setupSession() -> Bool {
+        //captureSession.sessionPreset = AVCaptureSession.Preset.high
+        
+        // Setup Camera
+        guard let camera = AVCaptureDevice.default(.builtInDualCamera, for: AVMediaType.video, position: .back) else { fatalError("No Depth Camera")}
+        
+        captureSession.sessionPreset = .photo
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: camera)
+            
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+                activeInput = input //this is not in the depth map
+            }
+        } catch {
+            print("Error setting device video input: \(error)")
+            return false
+        }
+        // 2.setup output
+        let output = AVCaptureVideoDataOutput()
+        output.setSampleBufferDelegate(self, queue: dataOutputQueue)
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+        
+        if captureSession.canAddOutput(output) {
+            captureSession.addOutput(output)
+        }
+        
+        let videoConnection = output.connection(with: .video)
+        videoConnection?.videoOrientation = .portrait
+        
+        // 3 lock AVCaptureDevice
+        do {
+            try camera.lockForConfiguration()
+            
+            // 4 Set minimum frame duration to be equal to teh supported frame rate of the depth data
+            if let frameDuration = camera.activeDepthDataFormat?
+                .videoSupportedFrameRateRanges.first?.minFrameDuration {
+                camera.activeVideoMinFrameDuration = frameDuration
+            }
+            
+            // 5 Unlock Configuration
+            camera.unlockForConfiguration()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        return true
+    }
+        
+    
+    func currentVideoOrientation() -> AVCaptureVideoOrientation {
+        var orientation: AVCaptureVideoOrientation
+        
+        switch UIDevice.current.orientation {
+        case .portrait:
+            orientation = AVCaptureVideoOrientation.portrait
+        case .landscapeRight:
+            orientation = AVCaptureVideoOrientation.landscapeLeft
+        case .portraitUpsideDown:
+            orientation = AVCaptureVideoOrientation.portraitUpsideDown
+        default:
+            orientation = AVCaptureVideoOrientation.landscapeRight
+        }
+        return orientation
+    }
+}
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+        
+        
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        let image = CIImage(cvPixelBuffer: pixelBuffer!)
+        
+        let previewImage: CIImage
+        previewImage = image
+        
+        
+        func convertCIImageToCGImage(inputImage: CIImage) -> CGImage? {
+            let context = CIContext(options: nil)
+            if let cgImage = context.createCGImage(inputImage, from: inputImage.extent) {
+                return cgImage
+            }
+            return nil
+        }
+        viewImage = convertCIImageToCGImage(inputImage: previewImage)
+        
+        let displayImage = UIImage(ciImage: previewImage)
+        DispatchQueue.main.async { [weak self] in
+            self?.camPreview.image = displayImage
+            
+        }
+    }
+}
+
+extension UIImageView {
+    func getPixelColorAtPoint(point: CGPoint, sourceView: UIImageView) -> UIColor {
+        let pixel = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 4)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        context!.translateBy(x: -point.x, y: -point.y)
+        
+        
+        sourceView.layer.render(in: context!)
+        let color: UIColor = UIColor(red: CGFloat(pixel[0])/255.0,
+                                     green: CGFloat(pixel[1])/255.0,
+                                     blue: CGFloat(pixel[2])/255.0,
+                                     alpha: CGFloat(pixel[3])/255.0)
+        
+        pixel.deallocate()
+    return color
+}
+}
+
+extension CGImage {
+    func pixelData() { //}-> [UInt8]? {
+        let dataSize = self.width * self.height * 4
+        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: &pixelData,
+                                width: Int(self.width),
+                                height: Int(self.height),
+                                bitsPerComponent: 8,
+                                bytesPerRow: 4 * Int(self.width),
+                                space: colorSpace,
+                                bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue)
+        //guard let cgImage = self.cgImage else { return nil }
+        context?.draw(self, in: CGRect(x: 0, y: 0, width: self.width, height: self.height))
+        print (pixelData.prefix(5))
+        print ("Width: \(self.width), Height: \(self.height)")
+        //return pixelData
+    }
+}
+
+extension CGImage {
+    
+    func pixelValues(fromCGImage imageRef: CGImage?) -> (pixelValues: [UInt8]?, width: Int, height: Int)
+    {
+        var width = 0
+        var height = 0
+        var pixelValues: [UInt8]?
+        if let imageRef = imageRef {
+            width = imageRef.width
+            height = imageRef.height
+            let bitsPerComponent = imageRef.bitsPerComponent
+            let bytesPerRow = imageRef.bytesPerRow
+            let totalBytes = height * bytesPerRow
+            
+            let colorSpace = CGColorSpaceCreateDeviceGray()
+            var intensities = [UInt8](repeating: 0, count: totalBytes)
+            
+            let contextRef = CGContext(data: &intensities, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: 0)
+            contextRef?.draw(imageRef, in: CGRect(x: 0.0, y: 0.0, width: CGFloat(width), height: CGFloat(height)))
+            
+            pixelValues = intensities
+        }
+        
+        return (pixelValues, width, height)
+    }
+    
+    func image(fromPixelValues pixelValues: [UInt8]?, width: Int, height: Int) -> CGImage?
+    {
+        var imageRef: CGImage?
+        if var pixelValues = pixelValues {
+            let bitsPerComponent = 8
+            let bytesPerPixel = 1
+            let bitsPerPixel = bytesPerPixel * bitsPerComponent
+            let bytesPerRow = bytesPerPixel * width
+            let totalBytes = height * bytesPerRow
+            
+            imageRef = withUnsafePointer(to: &pixelValues, {
+                ptr -> CGImage? in
+                var imageRef: CGImage?
+                let colorSpaceRef = CGColorSpaceCreateDeviceGray()
+                let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue).union(CGBitmapInfo())
+                let data = UnsafeRawPointer(ptr.pointee).assumingMemoryBound(to: UInt8.self)
+                let releaseData: CGDataProviderReleaseDataCallback = {
+                    (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+                }
+                
+                if let providerRef = CGDataProvider(dataInfo: nil, data: data, size: totalBytes, releaseData: releaseData) {
+                    imageRef = CGImage(width: width,
+                                       height: height,
+                                       bitsPerComponent: bitsPerComponent,
+                                       bitsPerPixel: bitsPerPixel,
+                                       bytesPerRow: bytesPerRow,
+                                       space: colorSpaceRef,
+                                       bitmapInfo: bitmapInfo,
+                                       provider: providerRef,
+                                       decode: nil,
+                                       shouldInterpolate: false,
+                                       intent: CGColorRenderingIntent.defaultIntent)
+                }
+                
+                return imageRef
+            })
+        }
+        
+        return imageRef
+    }
+}
